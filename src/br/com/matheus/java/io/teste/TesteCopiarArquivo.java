207cc92ee7b95896ead6bf1b5afc4ac77a9ed595
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-23 16:18:37 -0300

    Copia arquivo

---
+++ src/br/com/matheus/java/io/teste/TesteCopiarArquivo.java
@@ -0,0 +1,42 @@
+package br.com.matheus.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class TesteCopiarArquivo {
+
+	public static void main(String[] args) throws IOException{
+		InputStream fis = new FileInputStream("lorem.txt");
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osr = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osr);
+		
+		
+		String linha = br.readLine();
+		while(linha != null) {
+			bw.write(linha);
+			bw.newLine();
+			linha = br.readLine();
+		}
+		
+		
+		// close buffers
+		br.close();
+		bw.close();
+		
+		
+	}
+
+}
