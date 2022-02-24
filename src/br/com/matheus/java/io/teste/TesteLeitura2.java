04dd16e673d03e96f2ad2a84d2bea0f293428547
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-24 11:41:17 -0300

    USE string format and locale to format out print from scanner

--- src/br/com/matheus/java/io/teste/TesteLeitura2.java
+++ src/br/com/matheus/java/io/teste/TesteLeitura2.java
@@ -2,6 +2,7 @@ package br.com.matheus.java.io.teste;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -10,7 +11,33 @@ public class TesteLeitura2 {
 		
 		while(scan.hasNextLine()) {
 			String linha = scan.nextLine();
-			System.out.println(linha);
+			// Printa linha
+			//System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useDelimiter(",");
+			linhaScanner.useLocale(Locale.US);
+//			linhaScanner.useLocale(new Locale("pt", "BR"));
+			
+					
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+			
+			String formatado = String.format("Tipo de Conta:%s | Agencia:%04d | Número:%08d "
+					+ "| Titular:%s | Saldo:%05.2f", 
+					tipoConta, agencia, numero, titular, saldo);
+			
+//			System.out.println(formatado);			
+//			System.out.format("Tipo de Conta:%s | Agencia:%04d | Número:%08d "
+//					+ "| Titular:%15s | Saldo:%8.2f%n", 
+//					tipoConta, agencia, numero, titular, saldo);
+			System.out.format(Locale.US ,"Tipo de Conta:%s | Agencia:%04d | Número:%08d "
+					+ "| Titular:%15s | Saldo:%8.2f%n", 
+					tipoConta, agencia, numero, titular, saldo);
+			linhaScanner.close();
 		}
 		scan.close();
 		
